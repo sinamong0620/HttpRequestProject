391cb792f61dd08e6062d0cb60a969e402dcb835
sinamong0620 <sinaskim60@gmail.com>
2023-09-08 03:32:38 +0900

    async,await practice

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchmoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  //겉보기에는 단계적으로 실행되는 동기화 작업으로 보이지만 백그라운드에서는 아까 봤었던 then 호출로 코드가 변환되고 있다.
+  //여기에 async,await를 추가한 이유는 코드를 읽기 더 쉬워지기 때문이다.
+  async function fetchmoviesHandler() {
+    //fetch함수를 통해 해당 api에게 요청해서 response를 받아오는 것.
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    //해당 api에서 보내주는 객체가 이름이 달라서 우리 이름으로 바꿔준다음에 transform한 정보를 객체에다가 담아준다.
+    //그리고 그것을 movies에 저장한다!
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
   return (
     <React.Fragment>
